9147fa484ab5d4f34dcb3fa6b8cf22b053043638
rgrasielle <rgrasielle1@gmail.com>
2024-07-10 10:15:36 -0300

    primeiro commit

---
+++ classificadorDeNivel.js
@@ -0,0 +1,22 @@
+let nomeHeroi = "Zelda"
+let xp = 3000
+
+if (xp < 1000) {
+  nivel = "Ferro"
+} else if (xp > 1000 || xp <= 2000) {
+  nivel = "Bronze"
+} else if (xp > 2000 || xp <= 5000) {
+  nivel = "Prata"
+} else if (xp > 5000 || xp <= 7000) {
+  nivel = "Ouro"
+} else if (xp > 7000 || xp <= 8000) {
+  nivel = "Platina"
+} else if (xp > 8000 || xp <= 9000) {node
+  nivel = "Ascendente"
+} else if (xp > 9000 || xp <= 10000) {
+  nivel = "Imortal"
+} else if (xp > 10000) {
+  nivel = "Radiante"
+}
+
+console.log("O Herói de nome " + nomeHeroi + " está no nível " + nivel)
